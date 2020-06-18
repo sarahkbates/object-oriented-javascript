9f29b3b98439e7ec7ca19c4b59268f17d022a504
Sarah Bates <skbates00@me.com>
2020-06-18 10:33:28 -0500

    finsihed exercise 3, did bonus

--- oop3-abstract-morph.js
+++ oop3-abstract-morph.js
@@ -16,13 +16,86 @@ These sub-classes MUST implement the abstract methods of move() and act(), which
     -age
 
 */
+    console.log('Problem  1');
 
     class Creature {
         //your code here...
+
+        constructor(){
+            if(this.constructor == Creature){
+                throw new Error('Unable to instantiate Abstract Class Creature');
+            }
+        }
+
+        act(){
+            throw new Error('This is an abstract method. It cannot be invoked.');
+        }
+
+        move(){
+            throw new Error('This is an abstract method. It cannot be invoked.');
+        }
     }
 
     class Human extends Creature {
         //your code here...
+
+        constructor(fullName, currentLocation){
+            super();
+            this.fullName = fullName;
+            this.currentLocation = currentLocation;
+        }
+
+        act(){
+            console.log(`${this.fullName} is playing games.`);
+        }
+        move(newLocation){
+            console.log(`${this.fullName} is leaving ${this.currentLocation} and is going to ${newLocation}.`);
+        }
+
+        eat(food){
+            console.log(`${this.fullName} eats ${this.food}.`)
+        }
+    }
+
+    class Reptile extends Creature{
+        constructor(type, color, action){
+            super();
+            this.type = type;
+            this.color = color;
+            this.action = action;
+        }
+
+        act(){
+            console.log(`The ${this.type} is busy because it is ${this.action}.`)
+        }
+
+        move(){
+            console.log(`The ${this.type} is ${this.color}, so he needs to find other ${this.type}s that are also ${this.color}.`);
+        }
+
+        catchPrey(){
+            console.log(`The ${this.color} ${this.type} is going to catch prey now!`)
+        }
+    }
+
+    class Bear extends Creature{
+        constructor(furColor, habitat){
+            super();
+            this.furColor = furColor;
+            this.habitat = habitat;
+        }
+
+        act(){
+            console.log(`The ${this.furColor}bear is going for a walk.`);
+        }
+
+        move(){
+            console.log(`The ${this.furColor} bear lives in the ${this.habitat}.`);
+        }
+
+        fight(){
+            console.log(`The ${this.color} bear will fight other animals in the ${this.habitat}.`);
+        }
     }
 
 
@@ -32,6 +105,8 @@ These sub-classes MUST implement the abstract methods of move() and act(), which
 2. For the following Person class, modify the class methods, so the sub-class methods will successfully fire.  For the Teacher method, a set of methods have already been set up.  For the Student class, it will be up to you to determine how to set up the methods to fire in the Student sub-class.  :
 */
 
+console.log('Problem 2');
+
 class Person {
     name;
 
@@ -39,21 +114,21 @@ class Person {
         console.log(this.name + " is eating");
     }
 
-    sleep = () => {
+    sleep() {
         console.log(this.name + " is sleeping");
     }
 
-    code = function() {
+    code() {
         console.log(this.name + " is coding");
     }
 
-    repeat = function() {
+    repeat() {
         console.log(this.name + " is repeating the above steps, yet another time");
     }
 
     explain() {
         //this function should contain a console.log() explaining what you had to do to get the correct functions to work, and the reasoning behind what you did.
-        console.log("this explain method should contain explain what you had to do to get the correct functions to work, and the reasoning behind what you did.");
+        console.log("I made all of the fucntions in the Person class to prototypes, which will make it so the child's function will always run first. I also made all od the child functions into fields, because fields get checked first in JavaScript");
     }
 
 }
@@ -68,11 +143,11 @@ class Teacher extends Person {
         this.name = name;
     }
 
-    eat() {
+    eat = () => {
         console.log(this.name + " loves to teach before eating");
     }
 
-    sleep() {
+    sleep = () => {
         console.log(this.name + " sleeps after preparing the lesson plan");
     }
 
@@ -80,7 +155,7 @@ class Teacher extends Person {
         console.log(this.name + " codes first, then teaches it the next day.");
     }
 
-    repeat() {
+    repeat = () => {
         console.log(this.name + " teaches, codes, eats, sleeps, and repeats");
     }
 }
@@ -89,14 +164,32 @@ class Teacher extends Person {
 class Student extends Person {
     //set up your methods in this student class, so all of these methods will override the methods from the super class.
 
+    constructor(name){
+        super(name);
+        this.name = name;
+    }
+
     //eat method should print out - <stduent name> studies, then eats
+
+    eat = () => {
+        console.log(`${this.name} studies, then eats.`);
+    }
 
     //sleep method should print out, <student name> studies coding so much, that they dream about it in their sleep
 
+    sleep = () => {
+        console.log(`${this.name} studies coding so much, that they draem about it in their sleep.`);
+    }
     //code method should print out, <student name> was first overwhelmed by coding, but kept at it, and now has become a coding guru!
 
+    code = () => {
+        console.log(`${this.name} was first overwhelmed by coding, but kept at it, and now has become a coding guru!`);
+    }
     //repeat method should print out, <student name> keeps on studying, coding, eating, and sleeping, and puts it all on repeat.  
 
+    repeat = () => {
+        console.log(`${this.name} keeps on studying, coding, eating, and sleeping, and puts it all on repeat.`);
+    }
 }
 
 
@@ -137,7 +230,8 @@ class Cook {
     }
 
     explain = () => {
-        console.log("what could you do to get the prepare function to print out the food items that are being passed in to the function?  Once you figure it out, Write down your thought process in this explain method.");
+        // console.log("what could you do to get the prepare function to print out the food items that are being passed in to the function?  Once you figure it out, Write down your thought process in this explain method.");
+        console.log('You could switch the first prepare function, which takes the food items as an argument then into an arrow function or function expression, and switch the second prepare function to shorthand method defintion');
     }
 
 }
